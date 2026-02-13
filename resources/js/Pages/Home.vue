8a6b10bfdd8d0d682a469c55da073605ed04e932
Nicholas Ashley <ravenitsystems@outlook.com>
2026-02-13 18:24:49 +0000

    Initial Laravel & Vue build with only home page route configured

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<p>This is some test content for the AI to find</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/views/dashboard-app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Avantiy 2026 - Development</title>
+        <link rel="icon" href="images/favicon.png" />
+        @vite(['resources/js/app.js'])
+        @vite(['resources/css/app.css'])
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
